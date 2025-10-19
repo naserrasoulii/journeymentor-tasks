828107368d5f4392e0829926b9de7abcde7a8b6b
Naser Rasouli <naserrasouli21@gmail.com>
2025-10-19 10:39:02 +0330

    feat: add vue-query

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import '@/features/core/styles/style.css'
 import App from './App.vue'
 import { router } from '@/features/core/router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueQueryPlugin).mount('#app')
